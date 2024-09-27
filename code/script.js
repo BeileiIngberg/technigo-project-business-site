344f1f0249db25a6564f952ba81fdcd50d1a2968
Beilei Ingberg <beilei.ingberg@gmail.com>
2024-09-27 14:44:23 +0200

    updates

---
+++ code/script.js
@@ -0,0 +1,34 @@
+{
+    "args": {
+      
+    },
+    "data": "",
+    "files": {
+      
+    },
+    "form": {
+      
+    },
+    "headers": {
+      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+      "Accept-Encoding": "gzip, deflate, br, zstd",
+      "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+      "Host": "httpbin.org",
+      "Priority": "u=0, i",
+      "Referer": "https://technigo.disco.co/",
+      "Sec-Ch-Ua": "\"Chromium\";v=\"128\", \"Not;A=Brand\";v=\"24\", \"Google Chrome\";v=\"128\"",
+      "Sec-Ch-Ua-Mobile": "?0",
+      "Sec-Ch-Ua-Platform": "\"macOS\"",
+      "Sec-Fetch-Dest": "document",
+      "Sec-Fetch-Mode": "navigate",
+      "Sec-Fetch-Site": "cross-site",
+      "Sec-Fetch-User": "?1",
+      "Upgrade-Insecure-Requests": "1",
+      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
+      "X-Amzn-Trace-Id": "Root=1-66e3fac5-172d563074631eb54f11846b"
+    },
+    "json": null,
+    "method": "GET",
+    "origin": "31.208.91.195",
+    "url": "https://httpbin.org/anything"
+  }
